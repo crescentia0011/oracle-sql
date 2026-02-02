8058f24be85017d803621e89db09f0025442dd59
ByeongWan <crescentia0011@gmail.com>
2026-02-02 10:22:27 +0900

    첫번째 커밋

---
+++ SCOTT.sql
@@ -0,0 +1,13 @@
+--Orecle(DBMS) -  version(21c) - xe(database명)
+-- user(scott) - 테이블.
+-- Structured Query Language (SQL)
+SELECT studno, name --칼럼명(전체)
+FROM student; --테이블.
+
+-- 1.table = professor 테이블 전체 칼럼 조회
+SELECT * 
+FROM professor;
+
+
+
+
